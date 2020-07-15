650c51fbe153cb7c7d9c325fe55b008a84fdff24
ED-IP <edip_courses@outlook.es>
2020-07-15 07:29:47 +0000

    added placeholder code for emailJS

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,17 @@
+// this funciotn is not active yet, emailJS is not configured
+
+function sendMail(contactForm) {
+    emailjs.send("","",{
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailadress.value,
+        "project_request": contactForm.Form.projectsummary.value
+    }) 
+    .then(
+        function(response){
+            console.log("SUCCESS", response);
+        },
+        function(error){
+            console.log("FAILED", error);
+        });
+    return false;
+}
